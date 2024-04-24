069b8d71723b391232d0f1f7023529099a50e56d
Марина Романова <marina.simaganova@gmail.com>
2024-04-24 17:17:05 +0200

    header footer and css rules

---
+++ src/components/header/header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './header.css';
+import logo from '../../images/logo.png';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+
+
+const Header = () => {
+    return (
+        <div className="div-column">
+            <div className="top-menu div-row">
+                <div className="features-area div-row">
+                    <p>30-day return policy</p>
+                    <p>1000 active customers</p>
+                    <p>Free delivery</p>
+                </div>
+                <div className="currency-area div-row">
+                    <div className="currency-switcher div-row">
+                        <p>RSD</p>
+                        <p>EUR</p>
+                        <p>USD</p>
+                    </div>
+                </div>
+            </div>
+            <div className="main-menu div-row">
+                <div className="logo-area">
+                    <img src={logo} alt='logo' />
+                </div>
+                <div className="menu-area">
+                    <nav>
+                        <li><a href="#">All bikes</a></li>
+                        <li><a href='#'>City bikes</a></li>
+                        <li><a href="#">Road bikes</a></li>
+                    </nav>
+                </div>
+                <div className="cart-area">
+                    <div className="cart">
+                        <ShoppingBagOutlinedIcon sx={{ fontSize: 28 }}/>
+                        <p>15000din</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const inventoryData = [
+    { name: 'Ficus Lirata', price: 23.99, img: 'https://plantacia-spb.ru/wp-content/uploads/2022/03/9034-1-1-247x247.jpg' },
+    { name: 'Snake plant', price: 45.99, img: 'https://plantacia-spb.ru/wp-content/uploads/2021/05/2914-1-1-247x247.jpg' },
+    { name: 'Monstera', price: 17.99, img: 'https://plantacia-spb.ru/wp-content/uploads/2021/05/2003-1-2-247x247.jpg' },
+    { name: 'Howea', price: 17.99, img: 'https://plantacia-spb.ru/wp-content/uploads/2020/02/3801-1-2-247x247.jpg' },
+    { name: 'Cycas', price: 17.99, img: 'https://plantacia-spb.ru/wp-content/uploads/2019/08/2104-1-1-247x247.jpg' },
+    { name: 'pink Orchid', price: 49.99, img: 'https://plantacia-spb.ru/wp-content/uploads/2019/09/1510-14-1-247x247.jpg' },
+  ];
+  
+  export const currenciesData = ['USD', 'EUR', 'CAD'];
